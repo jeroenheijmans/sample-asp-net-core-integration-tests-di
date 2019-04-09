74131c990a5faf3bf30ea2932c3a847793d9b6a3
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-04-09 23:04:52 +0200

    Add solution loosely based on Scrutor

--- Foo.Web.Tests/IntegrationTestsFixture.cs
+++ Foo.Web.Tests/IntegrationTestsFixture.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,11 +15,39 @@ namespace Foo.Web.Tests
             
             builder.ConfigureTestServices(servicesConfiguration =>
             {
-                // TODO: Figure this out
-                // See also: https://stackoverflow.com/q/55599360/419956
-                servicesConfiguration.AddScoped<IBarService>(di
-                    => new DecoratedBarService(Server.Host.Services.GetRequiredService<IBarService>()));
-            });            
+                // Solution for: https://stackoverflow.com/q/55599360/419956
+                // See also other answers there, or in branches of the codebase.
+                // The chosen solution here is adapted from the "Scrutor" NuGet package, which
+                // is MIT licensed, and can be found at: https://github.com/khellang/Scrutor
+                //
+                // This solution might need further adaptation for thins like open generics...
+
+                var descriptor = servicesConfiguration.Single(s => s.ServiceType == typeof(IBarService));
+
+                servicesConfiguration.AddScoped<IBarService>(di 
+                    => new DecoratedBarService(GetInstance<IBarService>(di, descriptor)));
+            });
+        }
+
+        // Method loosely based on Scrutor, MIT licensed: https://github.com/khellang/Scrutor/blob/68787e28376c640589100f974a5b759444d955b3/src/Scrutor/ServiceCollectionExtensions.Decoration.cs#L319
+        private static T GetInstance<T>(IServiceProvider provider, ServiceDescriptor descriptor)
+        {
+            if (descriptor.ImplementationInstance != null)
+            {
+                return (T)descriptor.ImplementationInstance;
+            }
+
+            if (descriptor.ImplementationType != null)
+            {
+                return (T)ActivatorUtilities.CreateInstance(provider, descriptor.ImplementationType);
+            }
+
+            if (descriptor.ImplementationFactory != null)
+            {
+                return (T)descriptor.ImplementationFactory(provider);
+            }
+
+            throw new InvalidOperationException($"Could not create instance for {descriptor.ServiceType}");
         }
     }
 }
