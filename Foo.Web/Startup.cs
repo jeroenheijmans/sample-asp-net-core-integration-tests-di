32da0a7fcbc6ffc3bda1f13febe0b9f651b8b92e
Jeroen Heijmans <github@jeroenheijmans.nl>
2019-04-09 22:22:52 +0200

    Add workaround using extra DI registration

--- Foo.Web/Startup.cs
+++ Foo.Web/Startup.cs
@@ -21,6 +21,7 @@ namespace Foo.Web
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddScoped<IBarService, BarService>();
+            services.AddScoped<BarService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
